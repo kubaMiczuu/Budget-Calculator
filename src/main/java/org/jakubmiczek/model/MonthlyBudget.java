503798ddcef1f6fb1e4f912ab4d76d94097ed334
kubaMiczuu <kubamiczuu@gmail.com>
2026-05-20 15:33:57 +0200

    feat: add MonthlyBudget class with tests

---
+++ src/main/java/org/jakubmiczek/model/MonthlyBudget.java
@@ -0,0 +1,29 @@
+package org.jakubmiczek.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
+
+@Getter
+@Builder
+public class MonthlyBudget {
+    private final int year;
+    private final int month;
+    private final Map<TransactionCategory, BigDecimal> budget;
+
+    public static class MonthlyBudgetBuilder {
+        public MonthlyBudget build() {
+            if(year <=0 || year > LocalDate.now().getYear()) {
+                throw new IllegalArgumentException("[SYSTEM]: Year must be between 0 and now");
+            }
+            if(month <= 0 || month > 12) {
+                throw new IllegalArgumentException("[SYSTEM]: Month must be between 0 and 12");
+            }
+
+            return new MonthlyBudget(year,month,budget);
+        }
+    }
+}
---
+++ src/test/java/model/MothlyBudgetTest.java
@@ -0,0 +1,46 @@
+package model;
+
+import org.jakubmiczek.model.MonthlyBudget;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+public class MothlyBudgetTest {
+    @Test
+    void shouldThrowExceptionWhenYearIsLowerThanZero() {
+        assertThatThrownBy(() -> MonthlyBudget.builder().year(-1).build())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[SYSTEM]: Year must be between 0 and now");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenYearIsGraterThanCurrentYear() {
+        assertThatThrownBy(() -> MonthlyBudget.builder().year(LocalDate.now().plusYears(1).getYear()).build())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[SYSTEM]: Year must be between 0 and now");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenMonthIsLowerThanZero() {
+        assertThatThrownBy(() -> MonthlyBudget.builder().year(2026).month(-2).build())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[SYSTEM]: Month must be between 0 and 12");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenMonthIsGreaterThanTwelve() {
+        assertThatThrownBy(() -> MonthlyBudget.builder().year(2026).month(15).build())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[SYSTEM]: Month must be between 0 and 12");
+    }
+
+    @Test
+    void shouldCreateCorrectMonthlyBudget() {
+        MonthlyBudget monthlyBudget = MonthlyBudget.builder().year(2026).month(5).build();
+
+        assertThat(monthlyBudget.getMonth()).isEqualTo(5);
+        assertThat(monthlyBudget.getYear()).isEqualTo(2026);
+    }
+}
