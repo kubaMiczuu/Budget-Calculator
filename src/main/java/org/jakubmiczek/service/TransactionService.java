b74830172893b3211ac99dcbff560717ccc51c78
kubaMiczuu <kubamiczuu@gmail.com>
2026-05-24 14:46:24 +0200

    feat: implement TransactionService

--- src/main/java/org/jakubmiczek/service/TransactionService.java
+++ src/main/java/org/jakubmiczek/service/TransactionService.java
@@ -1,12 +1,15 @@
 package org.jakubmiczek.service;
 
+import org.jakubmiczek.exceptions.TransactionNotFoundException;
 import org.jakubmiczek.model.MonthlyBudget;
 import org.jakubmiczek.model.Transaction;
 import org.jakubmiczek.repository.TransactionRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 
 public class TransactionService {
@@ -24,6 +27,7 @@ public class TransactionService {
 
     public void addTransaction(Transaction transaction) {
         repository.save(transaction);
+        checkBudgetAlert();
     }
 
     private Optional<String> checkBudgetAlert() {
@@ -38,9 +42,30 @@ public class TransactionService {
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         if(expense.compareTo(budget) > 0) {
-            return Optional.of(String.format("Monthly limit was %s and was exceeded", budget));
+            return Optional.of(String.format("[SYSTEM]: Monthly limit was %s and was exceeded", budget));
         }
 
         return Optional.empty();
+    }
+
+    public void deleteTransaction(UUID id) {
+        if(repository.findById(id).isPresent()) {
+            repository.deleteById(id);
+        }
+        else {
+            throw new TransactionNotFoundException(id);
+        }
+    }
+
+    public Transaction findTransaction(UUID id) {
+        return  repository.findById(id).orElse(null);
+    }
+
+    public List<Transaction> getTransactions() {
+        return repository.findAll();
+    }
+
+    public void updateTransaction(Transaction transaction) {
+        repository.update(transaction);
     }
 }
